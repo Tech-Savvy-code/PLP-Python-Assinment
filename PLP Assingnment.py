b1dacb287d185a64a092579dd814198f86942df8
Tech-Savvy-code <opiyonick243@gmail.com>
2025-03-25 10:11:21 +0300

    Introduction Python Assignment

--- PLP Assingnment.py
+++ PLP Assingnment.py
@@ -23,6 +23,7 @@ def calculator():
                 print(f"{num1} * {num2} = {result}")
             elif operation == '/':
                 if num2 == 0:
+                    
                     print("Error: Division by zero is not allowed.")
                 else:
                     result = num1 / num2
